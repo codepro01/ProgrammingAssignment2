8cd74258447242f35a5b2d5f39bae5843a9d2f64
codepro01 <pankajnitrkl@yahoo.com>
2014-04-26 14:16:34 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function would create a Cached Matrix instance. This instance intenally is represented as a list
+## withs elements as setter for a matrix, getter for a matrix, setter for inverse and getter for inverse of matrix
+## values are set using these methods exposed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #
+  invMat <- NULL;
+  #setter to set the matrix value
+  set <- function(y)
+  {
+     x<<-y
+     
+     #ensure if new matrix is set old inverse value is set to NULL
+     invMat<<-NULL
+  }
+  
+  
+  #getter to get the stored matrix
+  get <-function() x
+  
+  
+  #setter to set the inverse of the matrix
+  setInverse <-function(inv_mat)
+  {
+    invMat<<- inv_mat;
+  }
+  
+  #getter of the inverser matrix if stored
+  #if not stored NULL would be returned
+  getInverse<-function() invMat;
+  
+  list(set=set, get=get,setInverse=setInverse, getInverse=getInverse);
 }
 
 
-## Write a short comment describing this function
+## This function would first validate if inverse of the matrix passed as input
+## is already computed. If the input is computed return the cached value , if not
+## inverse Matrix would be computed and stored.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #Check if the input special matrix has its inverse computed 
+  m<-x$getInverse();
+  
+  #return computed inverse if available
+  if(!is.null(m))
+  {
+    message("Inverse of matrix already cached, returning value");
+    return (m);
+  }
+  
+  #compute inverse and update the special matrix to store the cached 
+  #inverse
+  m<-solve(x$get());
+  x$setInverse(m);
+  m
 }
